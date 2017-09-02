c4c2def82f2016ba9129dd8216b0c4d871c0f504
John Desrosiers <johndrunner@yahoo.com>
2017-09-02 07:04:48 -0700

    added database.loadCards method. changed name of deck.getCards method to getAllCards

--- assets/javascript/deck.js
+++ assets/javascript/deck.js
@@ -12,25 +12,34 @@ firebase.initializeApp(config);
 /*
     ----- database -----
 
-    Reference to firbase.database object with added application
+    Reference to firebase.database object with added application
     specific properties and methods. *** Be careful not to override
     the names defined in the firebase api.
 */
 var database = firebase.database();
-var refCards = database.ref("cards");
+database.refCards = database.ref("cards");
 
-// initialize deck with cards from the database
-refCards.once("value", function(snapshot) {
-	snapshot.forEach(function(childSnap) {
-		deck.addCard(childSnap.val());
+// Adds cards in database to deck.
+database.loadCards = function(oDeck) {
+	// 	Parameters:
+	// 		fbDatabase: 	firebaset database reference
+	// 		oDeck:      	flash card deck
+
+	// get a snapshot from the firebase reference
+	// that points to the deck to use
+	database.refCards.once("value", function(snapshot) {
+		snapshot.forEach(function(childSnap) {
+
+			// add each child (card object) to the deck
+			oDeck.addCard(childSnap.val());
+		});
+		console.log(oDeck.getAllCards());
+
+	// log error with firebase request
+	}, function(err) {
+		console.log("Error loading cards from database:", err);
 	});
-	console.log(deck.getCards());
-
-}, function(err) {
-	console.log("Firebase Error:", err);
-});
-
-
+}
 
 
 /*
@@ -39,7 +48,6 @@ refCards.once("value", function(snapshot) {
 
 	Function for a flash card deck.
 */
-
 var deck = (function() {
 	var cards = [];
 
@@ -53,7 +61,7 @@ var deck = (function() {
 	addCard: function(card) {
 		cards.push(card);
 	},
-	getCards: function() {
+	getAllCards: function() {
 		return cards;
 	},
 
@@ -67,3 +75,8 @@ var deck = (function() {
 
 	};
 })();
+
+/*
+	Test Code
+	----------------------------------------------------------------*/
+database.loadCards(deck);
