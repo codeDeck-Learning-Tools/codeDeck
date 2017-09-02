46d0349e01d6b3e5c133126efb8def4ec6b1d108
Julie Ching <juliemching@gmail.com>
2017-09-01 22:01:07 -0700

    Update to Google Sign In to work

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,54 +1,39 @@
+
+$( '#signIn' ).on( 'click', click );
+
 // First, we perform the signInWithRedirect.
 // Creates the provider object.
+function click ( ) {
+    // Initialize Firebase
+    var config = {
+        'apiKey': 'AIzaSyCAGvW613Tfgyi6e7a8e1U1Nh45tSvPjCo',
+        'authDomain': 'codedeck-17e00.firebaseapp.com',
+        'databaseURL': 'https://codedeck-17e00.firebaseio.com',
+        'projectId': 'codedeck-17e00',
+        'storageBucket': 'codedeck-17e00.appspot.com',
+        'messagingSenderId': '82313729151'
+    };
+    firebase.initializeApp( config );
 
-var provider = new firebase.auth.GoogleAuthProvider();
+    var provider = new firebase.auth.GoogleAuthProvider();
 
-// You can add additional scopes to the provider:
-provider.addScope( 'email' );
+    // You can add additional scopes to the provider:
+    provider.addScope( 'email' );
 
-// Sign in with redirect:
-auth.signInWithRedirect( provider );
+    firebase.auth().signInWithPopup( provider ).then( function siPop ( result ) {
+        var token = result.credential.accessToken;
+        localStorage.setItem( 'token', token );
+        // The signed-in user info.
+        var user = result.user;
+        localStorage.setItem( 'user', user );
 
-/// /////////////////////////////////////////////////////////
-// The user is redirected to the provider's sign in flow...
-/// /////////////////////////////////////////////////////////
-
-// Then redirected back to the app, where we check the redirect result:
-auth.getRedirectResult().then( function redirectResult ( result ) {
-    // The firebase.User instance:
-    var user = result.user;
-
-    // The Google Sign In firebase.auth.AuthCredential containing the Google Sign In
-    // access token:
-    var credential = result.credential;
-
-    // As this API can be used for sign-in, linking and reauthentication,
-    // check the operationType to determine what triggered this redirect
-    // operation.
-    var operationType = result.operationType;
-}, function redirectOp ( error ) {
-    // The provider's account email, can be used in case of
-    // auth/account-exists-with-different-credential to fetch the providers
-    // linked to the email:
-    var email = error.email;
-
-    // The provider's credential:
-    var credential = error.credential;
-
-    // firebase.auth().signOut().then(function() {
-    // // Sign-out successful.   
-    // }).catch(function(error) {
-    // // An error happened.
-    // });
-
-    // In case of auth/account-exists-with-different-credential error,
-    // you can fetch the providers using this:
-    if ( error.code === 'auth/account-exists-with-different-credential' ) {
-        auth.fetchProvidersForEmail( email ).then( function fetch ( providers ) {
-
-            // The returned 'providers' is a list of the available providers
-            // linked to the email address. Please refer to the guide for a more
-            // complete explanation on how to recover from this error.
-        } );
-    }
-} );
+        /**
+         * This is where you will forward the user onto the next
+         * page.
+         */
+        document.location = '/deck.html';
+    } ).catch( function errorCB ( error ) {
+        alert( 'Authentication failed.' );
+        console.log( error );
+    } );
+};
