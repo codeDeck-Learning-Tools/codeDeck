9f12a9850bd060cb2f2502b25d19b595bd33941a
John Desrosiers <johndrunner@yahoo.com>
2017-09-02 10:26:04 -0700

    added methods to deck object and test code

--- assets/javascript/deck.js
+++ assets/javascript/deck.js
@@ -55,23 +55,45 @@ database.getCards = function(callback) {
 	Object for a flash card deck. May only have one deck at a time.
 */
 var deck = (function() {
+	var allCards = [];
 	var cards = [];	
+	var discarded = [];
 
 	return {
-		// --- public properties --- //
-
 		// --- public methods --- //
+
+		// adds a card to all cards and cards arrays
 		addCard: function(card) {
+			allCards.push(card);
 			cards.push(card);
 		},
 		// Returns array containing all card objects in deck
 		getAllCards: function() {
+			return allCards;
+		},
+
+		// Returns array containing current cards (not discarded)
+		getCurrentCards: function() {
 			return cards;
 		},
 
-		// sets the cards array
+		// Pops a card from the cards array and returns it.
+		popCard: function() {
+			var poppedCard = cards.pop();
+			discarded.push(poppedCard);
+			return poppedCard;
+		},
+
+		// Sets cards to discarded and empties discarded array.
+		reset: function() {
+			cards = allCards;
+			discarded = [];
+		},
+
+		// sets the allCards array and reset cards
 		setCards: function(arrCards) {
-			cards = arrCards;
+			allCards = arrCards;
+			this.reset();
 		},
 
 		// Randomizes the order of the cards. Returns true if succesful.
@@ -97,6 +119,7 @@ function renderColumn(colId, arrCards) {
 		$("<p>").text(this.front.text).appendTo("#" + colId);
 	})
 }
+/*
 // teset deck.shuffle();
 // get cards from database and show them before the shuffle on the left
 // and after the shuffle on the right.
@@ -105,3 +128,27 @@ database.getCards(function() {
 	deck.shuffle();
 	renderColumn("col2", deck.getAllCards());
 });
+*/
+/*
+// test deck.popCard()
+database.getCards(function() {
+	deck.shuffle();
+	renderColumn("col1", deck.getAllCards());
+	renderColumn("col2", [deck.popCard()]);
+	console.log(deck);
+
+});
+*/
+/*
+// test deck.reset()
+database.getCards(function() {
+	console.log(deck.getAllCards());
+	// pop 3 cards from the deck
+	for ( var i = 0; i < deck.getAllCards().length && i < 3; i++ ) {
+		console.log("popped card:", deck.popCard());
+	}
+	console.log(deck.getCurrentCards());
+	deck.reset();
+	console.log(deck.getCurrentCards());
+});
+*/
