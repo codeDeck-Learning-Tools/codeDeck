c29eca4d38783737d804126f798d4af6b8bd3d7c
Julie Ching <juliemching@gmail.com>
2017-09-03 00:04:56 -0700

    JS file changes and hooked up card display.
    Please notice that I have broken out the logic.js file back into separate component files due to the nature of how the different pages are. Also had to slightly rework the deck class. Now storing cards in localStorage after login and using them from that location. renderCard function is currently broken. Returning only an empty array.

--- assets/javascript/giphyAPI.js
+++ assets/javascript/giphyAPI.js
@@ -11,42 +11,39 @@
 // modify images object to have fixed format
 // run function when card counter function exceeds x
 
-
 var giphy = {
-	apiKey: "f3971dc19c6240feab39b26de85716d1",
-	host: "https://api.giphy.com",
-	limit: 3,
-	ratingLimit: "pg-13",
-	
-	search: function (searchString) {
-		var path = "/v1/gifs/search?";
-		var queryURL = "https://api.giphy.com/v1/gifs/search?";
+    'apiKey': 'f3971dc19c6240feab39b26de85716d1',
+    'host': 'https://api.giphy.com',
+    'limit': 3,
+    'ratingLimit': 'pg-13',
 
-		if (typeof searchString !== 'string'
-			|| searchString.length === 0) {
-			return false;
-		}
-		
-		queryURL += $.param({
-			api_key: this.apiKey,
-			q: searchString,
-			limit: this.limit,
-			rating: this.ratingLimit
-		});
+    'search': function ( searchString ) {
+        var path = '/v1/gifs/search?';
+        var queryURL = 'https://api.giphy.com/v1/gifs/search?';
 
-		return $.ajax({
-			url: queryURL,
-			method: "GET"
-		});
-	}
+        if ( typeof searchString !== 'string' ||
+   searchString.length === 0 ) {
+            return false;
+        }
+
+        queryURL += $.param( {
+            'api_key': this.apiKey,
+            'q': searchString,
+            'limit': this.limit,
+            'rating': this.ratingLimit
+        } );
+
+        return $.ajax( {
+            'url': queryURL,
+            'method': 'GET'
+        } );
+    }
 };
 
 // GET images object from within api-returned gif object, maybe fixed_width object (200px width, good for mobile)
 
 var gifRender = {
 
-}
-
+};
 
 // event handler. run function if card counter points count > x
-
