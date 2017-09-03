5f77491dd07212cdec227e28da6eae40c227d20c
John Desrosiers <johndrunner@yahoo.com>
2017-09-03 16:14:45 -0700

    moved renderExtract function to review.js

--- assets/javascript/review.js
+++ assets/javascript/review.js
@@ -150,3 +150,25 @@ $( document ).ready( function() {
         } );
 	}	
 } );
+
+
+/*
+    Functions for rendering the wikipedia api content
+    ----------------------------------------------------------
+*/
+// Returns html element for an extract object.
+function getExtractElement ( extract ) {
+    var $containerDiv = $( '<div>' );
+    var $link = $( '<a>' );
+    var $extract = $( '<p>' );
+
+    // set the href of the link and use the title for the
+    // text of the link
+    $link.attr( 'href', extract.url ).text( extract.title );
+
+    // text of extract goes in a p element
+    $extract.text( extract.text );
+
+    // append content and return the container element
+    return $containerDiv.append( [$link, $extract] ).get();
+}
