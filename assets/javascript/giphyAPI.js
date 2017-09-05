a4af4f7b9b881998c12aa25d951f921c7ce783d5
Philip Ma <pma@gmail.com>
2017-09-05 09:40:53 -0700

    function edits

--- assets/javascript/giphyAPI.js
+++ assets/javascript/giphyAPI.js
@@ -5,83 +5,86 @@
 // rating - (y, g, pg, pg-13, r)
 // fmt - return results in html or json format
 
-// pseudocode
-// make giphy object with set parameters for ajax call to giphy
-// GET url from 3 gif objects, pass to function
-// modify images object to have fixed format
-// run function when card counter function exceeds x
 
 /*
-	giphyAPI ajax, points counter function,
-	to be added to logic.js
-	giphyAPI 
+giphyAPI ajax, points counter function,
+to be added to logic.js
+giphyAPI 
 */
 
-
-$() () {
-var giphyAPI = {
+// object with giphy properties
+var giphyGIF = {
 	apiKey: "f3971dc19c6240feab39b26de85716d1",
 	host: "https://api.giphy.com/v1/gifs/search?q=",
 	limit: 3,
 	ratingLimit: "pg-13",
-	
-	// giphy method
-	search: function (searchString) {
+
+	search: function () {
 		var queryURL = "https://api.giphy.com/v1/gifs/search?";
-		
-		if (typeof searchString !== 'string'
-			|| searchString.length === 0) {
-				return false;
-			}
-			
-			queryURL += $.param({
-				api_key: this.apiKey,
-				q: searchString,
-				limit: this.limit,
-				rating: this.ratingLimit
-			});
-			
-			return $.ajax({
-				url: queryURL,
-				method: "GET"
-			})
-		
-			.done(function (response) {
-				var results = response.data;
-				
-				for (var i = 0; i < results.length; i++) {
-					if (results[i].rating == "pg-13" || results[i].rating == "g") {
-						var gifDiv = $("<div class='gif1'>");
-						var gifs = $("<img>");
-						gifs.attr("src", results[i].images.fixed_height.url);
-						gifDiv.append(gifs);
-					}
-				}
-		}
-		}
-	};
-	
-	// GET images object from within api-returned gif object, maybe fixed_width object (200px width, good for mobile)
-	
-	// var gifRender = {
-		
-	// }
-	
-	
-	// function to increment count variable. 
-	// once count > limit, calls giphyAPI function to play gifs
-	$(function () {
-		// initialize var count to 0
-		var count = 0;
-		// function to increment count when the next card button is clicked
-		// only set to next card. going back to a previous card won't add to the count
-		$("#nextCard").click(function () {
-			count++;
-			$("#nextCard").html("Current card count is: " + count);
+
+		// compile search url
+		queryURL += $.param({
+			api_key: this.apiKey,
+			q: "congratulations",
+			limit: this.limit,
+			rating: this.ratingLimit
 		});
 
-		if (count === 10) {
-			giphyGIF();
-			count = 0;
-		}
+		// return $.ajax({
+		// 	url: queryURL,
+		// 	method: "GET"
+		// });
+	},
+
+	// experimenting with fetch method, which should do the same task
+	render: $(function ($) {
+		fetch("https://api.giphy.com/v1/gifs/random?tag=congratulations&rating=pg-13&api_key=f3971dc19c6240feab39b26de85716d1&limit=3").then(function (response) {
+			return response.json();
+		}).then(function (result) {
+			$("myModal").html('<img src="' + result.data.image_url + '">');
+		});
 	});
+	// semi-colon syntax error?
+}
+
+// function giphyGIF() {
+// 	event.preventDefault();
+// 	/* id of html div for bootstrap modal
+// 	data-focus=""?
+// 	https://v4-alpha.getbootstrap.com/components/modal/ */
+
+// 	$("#myModal").load()
+// 	// jslint: Expected an identifier and instead saw '('.
+// 	// storing array of results
+
+// 	var results = response.data;
+
+// 	for (var i = 0; i < results.length; i++) {
+// 		if (results[i].rating == "pg-13" || results[i].rating == "g") {
+// 			var gifDiv = $("<div class='gif1'>");
+// 			var gifs = $("<img>");
+// 			gifs.attr("src", results[i].images.fixed_height.url);
+// 			gifDiv.append(gifs);
+// 		}
+// 	}
+// }
+// }
+// };
+
+// function to increment count variable. 
+// once count > limit, calls giphyAPI function to play gifs
+function counter() {
+	// initialize var count to 0
+	var count = 0;
+	// function to increment count when the next card button is clicked
+	// only set to next card. going back to a previous card won't add to the count
+	$("#nextCard").click(function () {
+		count++;
+		$("#nextCard").html("Current card count is: " + count);
+	});
+
+	if (count === 10) {
+		giphyGIF.render();
+		count = 0;
+	}
+});
