57432eb7deede6f1da1b82c9fcff81ce05fcc963
Julie Ching <juliemching@gmail.com>
2017-09-06 19:24:51 -0700

    Beautify documents

--- assets/javascript/testDeck.js
+++ assets/javascript/testDeck.js
@@ -6,20 +6,20 @@
 */
 
 // Switch the cards in local storage to a smaller deck for testing
-var testDeck = ( function() {
+var testDeck = (function () {
 	var storeKey = 'cards';
 
 	// save the current "full" deck in local storage
-	var sFullDeck = localStorage.getItem( storeKey );
+	var sFullDeck = localStorage.getItem(storeKey);
 
 	// put the first 3 cards into local storage
 	var arrSmallDeck = JSON.parse(sFullDeck).slice(0, 3);
-	localStorage.setItem( storeKey, JSON.stringify(arrSmallDeck) );
+	localStorage.setItem(storeKey, JSON.stringify(arrSmallDeck));
 
 	return {
 		// restore local storage to the initial state
-		restoreFullDeck: function() {
-			localStorage.setItem( storeKey, sFullDeck );
+		restoreFullDeck: function () {
+			localStorage.setItem(storeKey, sFullDeck);
 		},
 	}
-} )();
+})();
